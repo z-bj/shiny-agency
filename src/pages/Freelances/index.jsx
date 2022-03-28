678c0c8503d30a5533bd40426b6fc78dbde2f293
Bj'z <zbjarchi@gmail.com>
2022-03-28 18:12:40 +0000

    commit j-1B

---
+++ src/pages/Freelances/index.jsx
@@ -0,0 +1,19 @@
+import DefaultPicture from '../../assets/profile.png'
+
+const freelanceProfiles = [
+  {
+    name: 'Jane Doe',
+    jobTitle: 'Devops',
+    picture: DefaultPicture,
+  },
+  {
+    name: 'John Doe',
+    jobTitle: 'Developpeur frontend',
+    picture: DefaultPicture,
+  },
+  {
+    name: 'Jeanne Biche',
+    jobTitle: 'Développeuse Fullstack',
+    picture: DefaultPicture,
+  },
+]
