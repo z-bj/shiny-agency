c9792cb459cad0f2fd99a63fb5284bd80707a31b
Bj'z <zbjarchi@gmail.com>
2022-03-29 18:34:38 +0000

    commit j-2D

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,19 +1,39 @@
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const CardLabel = styled.span`
+  color: #5843e4;
+  font-size: 22px;
+  font-weight: bold;
+`
+
+const CardImage = styled.img`
+  height: 80px;
+  width: 80px;
+  border-radius: 50%;
+`
 
 function Card({ label, title, picture }) {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
-      <span>{label}</span>
-      <img src={picture} alt="freelance" height={80} width={80} />
+      <CardLabel>{label}</CardLabel>
+
+      <CardImage src={picture} alt="freelance" />
       <span>{title}</span>
     </div>
   )
 }
 
 Card.propTypes = {
-  label: PropTypes.string,
-  title: PropTypes.string,
-  picture: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+}
+
+Card.defaultProps = {
+  label: '',
+  title: '',
+  picture: 'DefaultPicture',
 }
 
 export default Card
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,19 @@
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const StyledLink = styled(Link)`
+  padding: 15px;
+  color: #8186a0;
+  text-decoration: none;
+  font-size: 18px;
+`
 
 function Header() {
   return (
     <nav>
-      <Link to="/">Acceuil</Link>
-      <Link to="/survey/3">Questionnaire</Link>
-      <Link to="/freelances">Freelances</Link>
+      <StyledLink to="/">Acceuil</StyledLink>
+      <StyledLink to="/survey/3">Questionnaire</StyledLink>
+      <StyledLink to="/freelances">Freelances</StyledLink>
     </nav>
   )
 }
