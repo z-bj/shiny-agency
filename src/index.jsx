5fd51805a65c12828d70a8cebb652c91e14cd458
Bj'z <zbjarchi@gmail.com>
2022-04-04 15:19:32 +0000

    commit j-4A

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { createGlobalStyle } from 'styled-components'
 import Error from './components/Error'
 import Header from './components/Header/index'
-import Freelances from './pages/Freelances'
-import Home from './pages/Home'
-import Results from './pages/Results'
-import Survey from './pages/Survey'
+import Freelances from './pages/Freelances/index'
+import Home from './pages/Home/index'
+import Results from './pages/Results/index'
+import Survey from './pages/Survey/index'
 
 const GlobalStyle = createGlobalStyle`
     div {
